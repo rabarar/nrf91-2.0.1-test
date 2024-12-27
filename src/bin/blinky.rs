d115c63f491c3f76e8bedd8f86242fff7670c1fe
Rob Baruch <robertbaruch@mac.com>
2024-12-27 16:06:45 +0000

    flash pattern on nrf9151-DK

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -9,14 +9,22 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P0_00, Level::Low, OutputDrive::Standard);
+    let led1 = Output::new(p.P0_00, Level::Low, OutputDrive::Standard);
+    let led2 = Output::new(p.P0_01, Level::Low, OutputDrive::Standard);
+    let led3 = Output::new(p.P0_04, Level::Low, OutputDrive::Standard);
+    let led4 = Output::new(p.P0_05, Level::Low, OutputDrive::Standard);
+
+    let mut leds = [(led1, "one", 100u64), (led2, "two", 200u64), (led3, "three", 300u64), (led4, "four", 400u64)];
 
     loop {
-        led.set_high();
-        defmt::info!("high");
-        Timer::after_millis(500).await;
-        led.set_low();
-        defmt::info!("low");
-        Timer::after_millis(1000).await;
+        for (led, label, delay) in &mut leds  {
+            led.set_high();
+            defmt::info!("high: {} ", label);
+            Timer::after_millis(*delay).await;
+            led.set_low();
+            defmt::info!("low: {} ", label);
+            Timer::after_millis(*delay/2u64).await;
+        }
     }
 }
+
